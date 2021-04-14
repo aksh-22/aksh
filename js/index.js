97b070228720918527b224fa8c215f2e34bb9db9
aksh-22 <akashkhandelwal1999@gmail.com>
2021-04-14 15:49:05 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,26 +1,35 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const navLinksMob = document.querySelectorAll('.nav__item-mob');
 const progress = document.querySelector('.progress-bars-wrapper');
 const progressBarPercents = [80, 75, 70, 65, 55, 50];
 const toggle = document.getElementById('toggle');
 const nav = document.getElementById('nav');
+const listItems = document.querySelectorAll('nav ul li');
+const sections = document.querySelectorAll('section');
 
 toggle.addEventListener('click', () => nav.classList.toggle('active'));
 
 navLinks.forEach((link) => {
 	link.addEventListener('click', () => {
-		// document.body.classList.remove('nav-open');
 		nav.classList.remove('active');
 	});
 });
 
-window.addEventListener('scroll', (e) => {
-	e.preventDefault();
+window.addEventListener('scroll', () => {
 	mainFn();
-	return false;
 });
 
 const mainFn = () => {
+	sections.forEach((section, i) => {
+		if (window.pageYOffset >= section.offsetTop - 10) {
+			navLinksMob.forEach((navE) => {
+				navE.classList.remove('active');
+			});
+			navLinksMob[i].classList.add('active');
+		}
+	});
+
 	if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
 		document.querySelectorAll('.progress-percent').forEach((el, i) => {
 			el.style.width = `${progressBarPercents[i]}%`;
@@ -31,7 +40,3 @@ const mainFn = () => {
 };
 
 mainFn();
-
-window.addEventListener('resize', () => {
-	// window.location.reload();
-});
